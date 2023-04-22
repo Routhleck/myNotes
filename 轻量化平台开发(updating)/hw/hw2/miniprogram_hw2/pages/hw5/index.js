194c4718e77ee9fcf53f161b46d19d89c313d490
Routhleck <1310722434@qq.com>
2023-04-22 20:58:27 +0800

    更新轻量化作业

--- 轻量化平台开发(updating)/hw/hw2/miniprogram_hw2/pages/hw5/index.js
+++ 轻量化平台开发(updating)/hw/hw2/miniprogram_hw2/pages/hw5/index.js
@@ -1,129 +1,66 @@
-// page/component/new-pages/cart/cart.js
+// pages/hw5_test/index.js
 Page({
-  data: {
-    carts:[],               // 购物车列表
-    hasList:false,          // 列表是否有数据
-    totalPrice:0,           // 总价，初始为0
-    selectAllStatus:true,    // 全选状态，默认全选
-    obj:{
-        name:"hello"
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
     }
-  },
-  onShow() {
-    this.setData({
-      hasList: true,
-      carts:[
-        {id:1,title:'新鲜芹菜 250g',image:'/images/5.jpg',num:5,price:2,selected:true},
-        {id:2,title:'五常大米 500g',image:'/images/6.jpg',num:2,price:4,selected:true},
-        {id:3,title:'iphone14',image:'/images/7.jpg',num:3,price:6599,selected:true},
-        {id:4,title:'暗影精灵6',image:'/images/8.jpg',num:2,price:5299,selected:true},
-        {id:5,title:'Giant捷安特PROPEL ADV SL 0',image:'/images/9.jpg',num:1,price:69899,selected:true},
-        {id:6,title:'京东京造机械键盘 jz990',image:'/images/10.jpg',num:5,price:599,selected:true},
-        {id:7,title:'医用N95口罩 一盒装',image:'/images/11.jpg',num:10,price:59,selected:true}
-      ]
-    });
-    this.getTotalPrice();
-  },
-  /**
-   * 当前商品选中事件
-   */
-  selectList(e) {
-    const index = e.currentTarget.dataset.index;
-    let carts = this.data.carts;
-    const selected = carts[index].selected;
-    carts[index].selected = !selected;
-    this.setData({
-      carts: carts
-    });
-    this.getTotalPrice();
-  },
-
-  /**
-   * 删除购物车当前商品
-   */
-  deleteList(e) {
-    const index = e.currentTarget.dataset.index;
-    let carts = this.data.carts;
-    carts.splice(index,1);
-    this.setData({
-      carts: carts
-    });
-    if(!carts.length){
-      this.setData({
-        hasList: false
-      });
-    }else{
-      this.getTotalPrice();
-    }
-  },
-
-  /**
-   * 购物车全选事件
-   */
-  selectAll(e) {
-    let selectAllStatus = this.data.selectAllStatus;
-    selectAllStatus = !selectAllStatus;
-    let carts = this.data.carts;
-
-    for (let i = 0; i < carts.length; i++) {
-      carts[i].selected = selectAllStatus;
-    }
-    this.setData({
-      selectAllStatus: selectAllStatus,
-      carts: carts
-    });
-    this.getTotalPrice();
-  },
-
-  /**
-   * 绑定加数量事件
-   */
-  addCount(e) {
-    const index = e.currentTarget.dataset.index;
-    let carts = this.data.carts;
-    let num = carts[index].num;
-    num = num + 1;
-    carts[index].num = num;
-    this.setData({
-      carts: carts
-    });
-    this.getTotalPrice();
-  },
-
-  /**
-   * 绑定减数量事件
-   */
-  minusCount(e) {
-    const index = e.currentTarget.dataset.index;
-    const obj = e.currentTarget.dataset.obj;
-    let carts = this.data.carts;
-    let num = carts[index].num;
-    if(num <= 1){
-      return false;
-    }
-    num = num - 1;
-    carts[index].num = num;
-    this.setData({
-      carts: carts
-    });
-    this.getTotalPrice();
-  },
-
-  /**
-   * 计算总价
-   */
-  getTotalPrice() {
-    let carts = this.data.carts;                  // 获取购物车列表
-    let total = 0;
-    for(let i = 0; i<carts.length; i++) {         // 循环列表得到每个数据
-      if(carts[i].selected) {                     // 判断选中才会计算价格
-        total += carts[i].num * carts[i].price;   // 所有价格加起来
-      }
-    }
-    this.setData({                                // 最后赋值到data中渲染到页面
-      carts: carts,
-      totalPrice: total.toFixed(2)
-    });
-  }
-
 })
