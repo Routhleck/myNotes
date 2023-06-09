58ca37d65022ce5ae54b2770bae4dc04fe488138
Routhleck <1310722434@qq.com>
2023-06-09 14:59:00 +0800

    更新轻量化作业

--- 轻量化平台开发(updating)/hw/hw3/Order/Client/exp.js
+++ 轻量化平台开发(updating)/hw/hw3/Order/Client/exp.js
@@ -142,6 +142,8 @@ app.post("/api/food/order",function(req,res){
 		}
 		else {
 			var orderlist = JSON.parse(data);
+			// 删除orderlist里的所有元素
+			orderlist.splice(0, orderlist.length);
 			orderlist.push(write_order);
 			fs.writeFile('order.json', JSON.stringify(orderlist), function (err) {
 				if (err) {
@@ -174,7 +176,63 @@ app.get("/api/food/order",function(req,res){
 
 //支付post请求
 app.post("/api/food/pay",function(req,res){
-   res.json({error:0,order_id:3})
+	order_id = req.body.order_id;
+	res.json({error:0,order_id:order_id})
+	var summoney = 0;
+	var date = '0';
+	var time = '0';
+	// 查看order.json, 计算总价summoney和时间ordertime, 同时时间格式为"ordertime": "2023/5/29 10:08:13"需要解析为date和time
+	fs.readFile('order.json', 'utf-8', function (err, data) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			var orderlist = JSON.parse(data);
+			console.log(orderlist[0]);
+			console.log(orderlist[0].foods);
+			// 通过每个foods的price和num计算summoney, 最后减去promotion的discount
+			for (var i = 0; i < orderlist[0].foods.length; i++) {
+				summoney += orderlist[0].foods[i].price * orderlist[0].foods[i].num;
+			}
+			summoney -= orderlist[0].promotion.discount;
+			// 计算时间
+			var ordertime = orderlist[0].ordertime;
+			date = ordertime.split(" ")[0];
+			time = ordertime.split(" ")[1];
+			time = time.split(":")[0] + ":" + time.split(":")[1];
+		}
+	});
+
+
+	// 查看record.json, 获取record中的record_id的最大值, 将record_id + 1, 写入到record.json中
+	fs.readFile('record.json', 'utf-8', function (err, data) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			var recordlist = JSON.parse(data);
+			record = recordlist.record;
+			var record_id = record[record.length - 1].record_id + 1;
+			var single_record = {
+				"record_id": record_id,
+				"summoney":  summoney,
+				"date": date,
+				"time": time
+			};
+			record.push(single_record);
+			recordlist.record = record;
+			fs.writeFile('record.json', JSON.stringify(recordlist), function (err) {
+				if (err) {
+					console.log(err);
+				}
+				else {
+					res.json({error:0,order_id:order_id});
+				}
+			});
+		}
+	});
+
+
 });
 
 //get请求消费记录
